8555c8746b2ba1a692bcc048dc7334cc4f496632
hjbc0921 <app9955@snu.ac.kr>
2018-04-27 02:00:21 +0900

    initial version of models.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
+    url(r'^',include('trips.urls')),
     path('admin/', admin.site.urls),
+    url(r'^api-auth/',include('rest_framework.urls')),
 ]
--- backend/trips/models.py
+++ backend/trips/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Trip(models.Model):
+    title       = models.CharField(max_length=50)
+    sinceWhen   = models.DateField()
+    tilWhen     = models.DateField()
+    users       = models.ManyToManyField(User)
+    #todoList    = models.ListField()
+    #ruleList    = models.ListField()
+    diary       = models.ForeignKey(Diary,related_name="diaries",on_delete=models.CASCADE) 
+    photo       = models.ForeignKey(Photo,related_name="photos",on_delete=models.CASCADE)
+
+class Expense(models.Model):
+    date        = models.DateField()
+    contents    = models.CharField(max_length=50)
+    money       = models.IntegerField()
+    spender     = models.ForeignKey(User,related_name="spender",on_delete=models.CASCADE)
+
+class Diary(models.Model):
+    contents    = models.TextField()
+    onePhoto    = models.ForeignKey(Photo,related_name="onePhoto",on_delete=models.CASCADE)
+    writer      = models.ForeignKey(User,related_name="writer",on_delete=models.CASCADE)
+    date        = models.DateField()
+
+class Photo(models.Model):
+    contents    = models.TextField()
+    image       = models.ImageField()
+    folder      = models.CharField(max_length=20)
+    date        = models.DateField()
+    
+
+
+
+
