b083ae51000ef68b7ca8d89f5f4e9a36bddf380d
hjbc0921 <app9955@snu.ac.kr>
2018-04-28 02:06:30 +0900

    create token when new user is made - models.py

--- backend/trips/models.py
+++ backend/trips/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 class Trip(models.Model):
     title       = models.CharField(max_length=50)
@@ -51,8 +55,8 @@ class Marker(models.Model):
     place       = models.CharField(max_length=200)
     tripID      = models.ForeignKey(Trip,related_name="trip_marker",on_delete=models.CASCADE)
 
-#@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-#def create_auth_token(sender,instance=None,created=False,**kwargs):
-#    if created:
-#        Token.objects.create(user=instance)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender,instance=None,created=False,**kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
