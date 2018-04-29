884c10582b8aca26fc847a23841c23ab9b154836
choppaluv <rlawldn1016@naver.com>
2018-04-29 23:53:37 +0900

    added homepage after login

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.views.generic.base import TemplateView
 from rest_framework.authtoken import views
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'api/',include('trips.urls')),
     path('admin/', admin.site.urls),
     url(r'^api-token-auth/',views.obtain_auth_token),
