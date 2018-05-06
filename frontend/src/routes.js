cac9c8c0c091a4456f9edbd958a48efd39eee259
hjbc0921 <app9955@snu.ac.kr>
2018-05-06 14:10:23 +0900

    route rules page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router'
 
 import App from 'components/App'
 import { HomePage } from 'components'
+import AddRule from './containers/AddRule'
 
 const routes = (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="/rules" component={AddRule} />
   </Route>
 )
 
