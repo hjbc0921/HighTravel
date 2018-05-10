503083555095e460d9d68299db88cda2b5740652
parkkyuyeon <715pky@naver.com>
2018-05-10 15:51:19 +0000

    make Intro structure in container and store

---
+++ frontend/src/store/intro/actions.js
@@ -0,0 +1,27 @@
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const USER_INFO_RECEIVED = 'INTRO_RECEIVED';
+export const LOGIN_FAILED = 'LOGIN_FAILED';
+
+export const IntroReceived = ({username, password}) => {
+  return {
+    type: INTRO_RECEIVED,
+    username: username,
+    password: password
+  }
+};
+
+export const loginFailed = (err) => {
+  return {
+    type: LOGIN_FAILED,
+    errorMessage: err
+  }
+};
+
+export const loginRequest = (username, password) => {
+  return {
+    type: LOGIN_REQUEST,
+    username,
+    password
+  }
+};
