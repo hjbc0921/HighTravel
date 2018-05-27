9109a6ce985610f15bef352777b21d8f7267a53d
hjbc0921 <app9955@snu.ac.kr>
2018-05-27 18:29:50 +0900

    modify or delete budget implemented

--- frontend/src/store/budget/actions.js
+++ frontend/src/store/budget/actions.js
@@ -2,6 +2,22 @@ export const ADDBUDGET_REQUEST = 'ADDBUDGET_REQUEST';
 export const ADDBUDGET_FAIL = "ADDBUDGET_FAIL"
 export const ADDBUDGET_SUCCESS = "ADDBUDGET_SUCCESS"
 export const LOAD_BUDGET = "LOAD_BUDGET"
+export const CHANGE_CONTENT = "CHANGE_CONTENT"
+export const DELETE_ROWS = "DELETE_ROWS"
+
+export const changeContent = (idUpdatedRow) => {
+  return {
+  type : CHANGE_CONTENT,
+  idUpdatedRow
+  }
+}
+
+export const deleteRows = (budIDs) => {
+  return{
+  type : DELETE_ROWS,
+  budIDs
+  }
+}
 
 export const loadBudget = (tripBudgets) => {
   return{
