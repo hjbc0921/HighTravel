1e1817c2d0f4645dc8fa7f7c35e7b6086fad304d
swpp <narajoa21@gmail.com>
2018-05-27 15:31:27 +0900

    modify photos with example photos

--- frontend/src/components/molecules/Photos/index.js
+++ frontend/src/components/molecules/Photos/index.js
@@ -1,23 +1,42 @@
 import React, { PropTypes } from 'react'
 import styled from 'styled-components'
 import { font, palette } from 'styled-theme'
+import Gallery from 'react-photo-gallery';
 
 const Wrapper = styled.div`
   font-family: ${font('primary')};
   color: ${palette('grayscale', 0)};
 `
 
-const Photos = ({ children, ...props }) => {
+const PhotoList = () => {
+  const Foldername ='example'
+  const Photo = [{
+ src:  'http://travel.chosun.com/site/data/img_dir/2017/06/30/2017063001239_0.jpg',
+ width:10,
+ height:10,
+},
+  {
+    src :'https://img-wishbeen.akamaized.net/plan/1454465238030_15657083522_d45a489b15_b.jpg',
+    width:10,
+    height:10	,
+}
+ ];
+
   return (
-    <Wrapper {...props}>
-      {children}
-    </Wrapper>
+    <div>
+      <Foldername/>
+      <Gallery photos={Photo}/>
+    </div>
   )
 }
 
-Photos.propTypes = {
-  reverse: PropTypes.bool,
-  children: PropTypes.node,
+PhotoList.propTypes = {
+  Foldername: PropTypes.string,
+  Photo: PropTypes.arrayOf(PropTypes.shape({
+  src:PropTypes.string.isRequired,
+  width:PropTypes.number.isRequired,
+  height:PropTypes.number.isRequired
+ }))
 }
 
-export default Photos
+export default PhotoList
