c6131af0011fd7e74f794f4b5a9a53dd9642ff9b
swpp <narajoa21@gmail.com>
2018-05-28 21:03:31 +0900

    jest

--- frontend/src/components/molecules/Calendar/index.test.js
+++ frontend/src/components/molecules/Calendar/index.test.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Calendar from '.'
+import AddSchedule from './index'
 
-const wrap = (props = {}) => shallow(<Calendar {...props} />)
+const onShowSchedule = jest.fn()
 
-it('renders children when passed in', () => {
-  const wrapper = wrap({ children: 'test' })
-  expect(wrapper.contains('test')).toBe(true)
-})
-
-it('renders props when passed in', () => {
-  const wrapper = wrap({ id: 'foo' })
-  expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
-})
+it('calls onShowSchedule when Clicked', () =>{
+   onShowSchedule.mockClear()
+   expect(onShowSchedule).not.toBeCalled()
+}) 
--- frontend/src/components/molecules/SignUp/index.test.js
+++ frontend/src/components/molecules/SignUp/index.test.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import SignUp from '.'
+import SignUp from './index'
 
-const wrap = (props = {}) => shallow(<SignUp {...props} />)
+const onSignUp= jest.fn()
 
-it('renders children when passed in', () => {
-  const wrapper = wrap({ children: 'test' })
-  expect(wrapper.contains('test')).toBe(true)
-})
-
-it('renders props when passed in', () => {
-  const wrapper = wrap({ id: 'foo' })
-  expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
-})
+it('calls onAddSchedule when Clicked', () =>{
+   onSignUp.mockClear()
+   expect(onSignUp).not.toBeCalled()
+}) 
