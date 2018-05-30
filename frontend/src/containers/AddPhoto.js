0e77e7b0cf7b57650b98eb1550873bec692167cf
choppaluv <rlawldn1016@naver.com>
2018-05-30 15:47:32 +0900

    Add Photo POST success

--- frontend/src/containers/AddPhoto.js
+++ frontend/src/containers/AddPhoto.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux'
 import { AddPhoto } from "../components/molecules/AddPhoto";
-import { addphotoRequest } from "../store/addphoto/actions";
+//import { addphotoRequest } from "../store/addphoto/actions";
 
 const mapStateToProps = (state) => {
    return{
@@ -11,7 +11,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
    return{
       onAddPhoto: (selectedFile,text) =>{
-           dispatch(addphotoRequest(selectedFile,text))
+           // dispatch(addphotoRequest(selectedFile,text))
       }
    }
 };
